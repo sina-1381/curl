2f496e6a2ec51eb4e077a1cd2f404a925a785d5d
sina darestany <sina.darestany@gmail.com>
2021-09-01 17:47:26 +0430

    first commit

--- main.go
+++ main.go
@@ -3,16 +3,13 @@ package main
 import (
 	"curl/controller"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"os"
 )
 
 func main() {
 	router := gin.Default()
-	err := godotenv.Load(".env")
+	router.POST("/curl" , controller.Dextools)
+	err := router.Run(":8080")
 	if err != nil{
 		panic(err)
 	}
-	router.POST("/curl" , controller.Dextools)
-	err = router.Run(os.Getenv("PORT"))
 }
